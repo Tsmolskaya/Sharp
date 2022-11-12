8ed2d8a375b497baa89214d7b12a8bb037c63030
Shkodka <smolskayatatyana@yandex.ru>
2022-11-13 00:07:31 +0300

    Задача 2 решена

---
+++ Task_002/Program.cs
@@ -0,0 +1,28 @@
+﻿// Составить программу вывода на экран «столбиком» 
+//пяти любых чисел, введенных пользователем с клавиатуры.
+
+//Console.WriteLine("Hello, World!");
+
+int firstNumber;
+int secondNumber;
+int thirdNumber;
+int fourNumber;
+int fiveNumber;
+
+Console.WriteLine("Введите 1 число: ");
+firstNumber = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine("Введите 2 число: ");
+secondNumber = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine("Введите 3 число: ");
+thirdNumber = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine("Введите 4 число: ");
+fourNumber = Convert.ToInt32(Console.ReadLine ());
+Console.WriteLine("Введите 5 число: ");
+fiveNumber = Convert.ToInt32(Console.ReadLine ());
+
+Console.WriteLine("Выводим введённые числа: ");
+Console.WriteLine("firstNumber");
+Console.WriteLine("secondNumber");
+Console.WriteLine("thirdNumber");
+Console.WriteLine("fourNumber");
+Console.WriteLine("fiveNumber");
