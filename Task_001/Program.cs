85f2ab659ac345abd2ab74bc127adebe136455c1
Shkodka <smolskayatatyana@yandex.ru>
2022-11-12 23:36:47 +0300

    Задача 1 решена

--- Task_001/Program.cs
+++ Task_001/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Составить программу вывода на экран в одну строку 
+//четырех любых чисел с тремя пробелами между ними.
+//Создание объекта для генерации чисел
+Random rnd = new Random();
+  
+int value = rnd.Next();  //Получить очередное случайное число
+int value1 = rnd.Next(); //Получить очередное случайное число
+int value2 = rnd.Next(); //Получить очередное случайное число
+int value3 = rnd.Next(); //Получить очередное случайное число
+  
+Console.WriteLine(value + "   " + value1 + "   " + value2+ "   "+value3+ "   " +value3);  //Вывод числа в консоль
